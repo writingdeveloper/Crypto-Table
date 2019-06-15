dc14f7486685ea5a4d422b50709f01ed14fd1cf1
Si Hyeong Lee <super2451894@gmail.com>
2019-06-15 23:16:40 +0900

    React Project Init
    - React Project Init

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, {
+  Component
+} from 'react';
+import Header from './components/Header'
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+  }
+  render() {
+    return ( <
+      Header > < /Header>
+    )
+  }
+}
+
+export default App;
