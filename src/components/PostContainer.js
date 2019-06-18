21aac7017ca48909f4300f39b92ee7bc9d7280fd
Si Hyeong Lee <super2451894@gmail.com>
2019-06-18 12:49:08 +0900

    Update PostContainer.js
    - API Data get Update

--- src/components/PostContainer.js
+++ src/components/PostContainer.js
@@ -1,77 +1,23 @@
 import React, { Component } from 'react';
-// import axios from 'axios';
 import './PostContainer.css';
 
 class PostContainer extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     data: chartData,
-  //   };
-  // }
-
-  // async componentDidMount() {
-  //   let { data: chartData } = await axios.get(
-  //     'https://api.bithumb.com/public/ticker/all'
-  //   );
-  //   this.setState({ chartData });
-  // }
-
-  // componentDidMount() {
-  //   var th = this;
-  //   this.serverRequest = axios
-  //     .get('https://api.bithumb.com/public/ticker/all')
-  //     .then(function(event) {
-  //       th.setState({
-  //         data: event.data,
-  //       });
-  //     });
-  // }
-
-  // componentWillUnmount() {
-  //   this.serverRequest.abort();
-  // }
-
   render() {
-    // const { chartData } = this.state;
-    // this.state.data.date = undefined;
-    // delete this.state.data.date;
-    // console.log(chartData.data);
-
-    // for (const val, index in chartData.data) {
-    //   console.log(`${index} : ${val.sell_price}`)
-    // }
-
-    // Object.entries(chartData.data).forEach(([key, value]) => {
-    //   console.log(value);
-    // });
-    // for (const [key, value] of Object.entries(chartData.data)) {
-    //   console.log(`${value.sell_price}`);
-    // }
-
-    // let contents = this.state.data.data;
-    // let newData = contents;
-    // console.log(Object.keys(newData));
-    // var lists = [];
-
-    var data = this.props.data;
-    var chart;
+    let data = this.props.data;
+    let chart = [];
     // console.log(data);
     if (data.status === '0000') {
-      console.log(Object.keys(data.data));
-      // data.
-
-      for (const [key, value] of Object.entries(data.data)) {
-        // console.log(`${value.sell_price}`);
-        chart += `<tr>
-            <td>${key}</td>
-            <td>${value.sell_price}</td>
-            <td>${value.volume_7day}</td>
-          </tr>`;
+      delete data.data['date'];
+      for (let [key, value] of Object.entries(data.data)) {
+        chart.push(
+          <tr key={key}>
+            <td>{key}</td>
+            <td>{value.sell_price}</td>
+            <td>{value.volume_7day}</td>
+          </tr>
+        );
       }
-      console.log(chart);
     }
-
     return (
       <div className="Post">
         <table id="table" className="table table-striped table-bordered">
@@ -82,7 +28,7 @@ class PostContainer extends Component {
               <th>Volume</th>
             </tr>
           </thead>
-          <tbody>{(dangerouslySetInnerHTML = chart)}</tbody>
+          <tbody>{chart}</tbody>
         </table>
       </div>
     );
