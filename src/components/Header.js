e07a07791f12bba489ec56b9e604702af969b7a3
Si Hyeong Lee <super2451894@gmail.com>
2019-06-18 13:41:13 +0900

    Update Header.js
    - Name FIx

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import './Header.css';
 
 class Header extends Component {
   render() {
-    return <div className="Header">Crypto Chart</div>;
+    return <div className="Header">Crypto Table</div>;
   }
 }
 
