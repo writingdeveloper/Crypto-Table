4d911f0ecafa884718671ecb119f4187aed9faa9
Si Hyeong Lee <super2451894@gmail.com>
2019-06-25 15:19:20 +0900

    Fix some Changes
    - Remove useless Comments
    - Fix CSS position

--- src/components/PostContainer.js
+++ src/components/PostContainer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import DataTable from 'react-data-table-component';
 import axios from 'axios';
+import 'semantic-ui-css/semantic.min.css';
 import './PostContainer.css';
 import { darkTheme, columns } from './tableSetting';
 
@@ -12,46 +13,10 @@ class PostContainer extends Component {
   };
 
   async componentDidMount() {
-    // const response = await axios.get(
-    //   `https://api.bithumb.com/public/ticker/all`
-    // );
-    // console.log(response.data);
-
-    // let chart = [];
-    // let chartLength;
-    // // console.log(data);
-    // if (response.data.status === '0000') {
-    //   delete response.data.data['date'];
-    //   console.log(response.data.data);
-    //   for (let [key, value] of Object.entries(response.data.data)) {
-    //     if (value['24H_fluctate_rate'] === 1) {
-    //       chart.push({
-    //         key: key,
-    //         Price: `${value.sell_price}원`,
-    //         FluctateRate: `+ ${value['24H_fluctate_rate']}`,
-    //         Volume: value.volume_7day,
-    //       });
-    //     } else {
-    //       chart.push({
-    //         key: key,
-    //         Price: `${value.sell_price}원`,
-    //         FluctateRate: value['24H_fluctate_rate'],
-    //         Volume: value.volume_7day,
-    //       });
-    //     }
-    //   }
-    //   chartLength = chart.length;
-
     this.interval = setInterval(() => {
       this.getData();
     }, 1000);
-
-    // this.setState({
-    //   data: chart,
-    //   totalRows: chartLength,
-    // });
   }
-  // }
 
   async getData() {
     const response = await axios.get(
