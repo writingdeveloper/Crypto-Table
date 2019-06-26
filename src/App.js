f2e6b3c20358ca9ebcffd60fda281b1f7db64fe3
Si Hyeong Lee <super2451894@gmail.com>
2019-06-26 14:17:19 +0900

    Update App.js
    - Remove Semantic UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
-import 'semantic-ui-css/semantic.min.css';
 import PostContainer from './components/PostContainer';
 import './App.css';
 
