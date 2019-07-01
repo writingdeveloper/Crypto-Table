037a36b1ed78abbc6a82520a942da7af27ac6ca7
Si Hyeong Lee <super2451894@gmail.com>
2019-07-01 21:58:37 +0900

    Update tableSetting.js
    - Responsive Page Update

--- src/components/tableSetting.js
+++ src/components/tableSetting.js
@@ -42,6 +42,8 @@ const columns = [
     selector: 'key',
     sortable: true,
     ignoreRowClick: true,
+    width: '16%',
+    // maxWidth: '5%',
     cell: row => (
       <a
         href={'https://www.bithumb.com/trade/order/' + row.key}
@@ -56,6 +58,8 @@ const columns = [
     selector: 'Price',
     sortable: true,
     right: true,
+    width: '25%',
+    // maxWidth: '20%',
   },
   {
     name: '24Hours Fluctate',
@@ -66,13 +70,13 @@ const columns = [
     cell: row => {
       if (row.FluctateRate < 0) {
         return (
-          <div class="minus">
+          <div className="minus">
             {row.FluctateRate}% ({row.FluctateRate24}원)
           </div>
         );
       } else {
         return (
-          <div class="plus">
+          <div className="plus">
             +{row.FluctateRate}% (+{row.FluctateRate24}원)
           </div>
         );
