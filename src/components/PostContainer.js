088e7d3377d8fdaa530d31c4680b301a65416d40
Si Hyeong Lee <super2451894@gmail.com>
2019-07-03 17:07:21 +0900

    Update PostContainer.js
    - Update API Statue fail UI

--- src/components/PostContainer.js
+++ src/components/PostContainer.js
@@ -7,6 +7,8 @@ import { darkTheme, columns } from './tableSetting';
 /* Component */
 class PostContainer extends Component {
   state = {
+    title: <div>Load data from API Server...</div>,
+    status: <div>Wait</div>,
     data: [],
     totalRows: 86,
   };
@@ -24,9 +26,13 @@ class PostContainer extends Component {
     // console.log(response.data);
 
     let chart = [];
+    let status;
+    /* If API Status Success */
     if (response.data.status === '0000') {
       delete response.data.data['date'];
+
       // console.log(response.data.data)
+      // console.log(Object.keys(response.data.data));
       for (let [key, value] of Object.entries(response.data.data)) {
         if (Math.sign(value['24H_fluctate_rate']) === 1) {
           chart.push({
@@ -46,41 +52,58 @@ class PostContainer extends Component {
           });
         }
       }
+
       this.setState({
+        statue: status,
+        result: 'success',
         data: chart,
         totalRows: chart.length,
+        title: (
+          <div>
+            <div className="logoContainer">
+              <a href="https://www.bithumb.com/">
+                <img
+                  src="/images/bithumb.png"
+                  alt="bithumb LOGO"
+                  className="logo"
+                />
+              </a>
+            </div>
+            <div id="statusSuccess">{status}</div>
+            <p className="apiSuccess"> API Works Fine</p>
+          </div>
+        ),
+      });
+    } else {
+      /* If Server Status Fails */
+      this.setState({
+        statue: status,
+        result: 'fail',
+        title: (
+          <div>
+            <div className="logoContainer">
+              <a href="https://www.bithumb.com/">
+                <img
+                  src="/images/bithumb.png"
+                  alt="bithumb LOGO"
+                  className="logo"
+                />
+              </a>
+            </div>
+            <div id="statusFail">{status}</div>
+            <p className="apiFail"> API is not wokring. Something is Wrong</p>
+          </div>
+        ),
       });
     }
   }
 
-  // for (let [key, value] of Object.entries(response.data.data)) {
-  //   if (Math.sign(value['24H_fluctate_rate']) === 1) {
-  //     chart.push({
-  //       key: key,
-  //       Price: `${value.sell_price}원`,
-  //       FluctateRate: `+ ${value['24H_fluctate_rate']}% (${
-  //         value['24H_fluctate']
-  //       }원)`,
-  //       Volume: `${Number(value.volume_7day).toFixed(5)} ${key}`,
-  //     });
-  //   } else {
-  //     chart.push({
-  //       key: key,
-  //       Price: `${value.sell_price}원`,
-  //       FluctateRate: `${value['24H_fluctate_rate']}% (${
-  //         value['24H_fluctate']
-  //       }원)`,
-  //       Volume: `${Number(value.volume_7day).toFixed(5)} ${key}`,
-  //     });
-  //   }
-  // }
-
   render() {
-    const { data, totalRows } = this.state;
+    const { data, totalRows, title } = this.state;
     // console.log(data);
     return (
       <DataTable
-        noHeader
+        title={title}
         className="Post"
         columns={columns}
         data={data}
